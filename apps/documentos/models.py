b04c5672246c3f5a9315e22cb4695c03513aeb57
Andre Guilherme de Oliveira Alcantara <andre.alcantara@reitoria.ifpe.edu.br>
2022-01-12 17:05:04 -0300

    Adicionando create and list de documento, ainda nao funciona.

--- apps/documentos/models.py
+++ apps/documentos/models.py
@@ -1,10 +1,16 @@
 from django.db import models
+from django.urls import reverse
+
 from apps.funcionarios.models import Funcionario
 
 
 class Documento(models.Model):
     descricao = models.CharField(max_length=100)
+    arquivo = models.FileField(upload_to='documentos')
     pertence = models.ForeignKey(Funcionario, on_delete=models.deletion.PROTECT)
 
     def __str__(self):
         return self.descricao
+
+    def get_absolute_url(self):
+        return reverse('editar_funcionarios', args=[self.pertence.id])
--- apps/documentos/views.py
+++ apps/documentos/views.py
@@ -1,3 +1,32 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.views.generic import CreateView, ListView, DeleteView
+
+from apps.documentos.models import Documento
+from apps.funcionarios.models import Funcionario
+
+
+class DocumentosList(ListView):
+    model = Documento
+    def get_queryset(self):
+        return Documento.objects.filter(pertence=Funcionario.objects.filter(id=self.request.get['pk']))
+
+class DocumentosDelete(DeleteView):
+    model = Documento
+
+
+class DocumentosCreate(CreateView):
+    model = Documento
+    fields = ['descricao', 'arquivo']
+
+    def post(self, request, *args, **kwargs):
+        form = self.get_form()
+        id_funcionario = self.kwargs['id_funcionario']
+        form.instance.pertence = Funcionario.objects.filter(id_funcionario)
+        form.instance.pertence_id = form.instance.pertence.id
+        if form.is_valid():
+            return self.form_valid(form)
+        else:
+            return self.form_invalid(form)
+
