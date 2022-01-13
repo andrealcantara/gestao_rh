6df465062241f6626aa059f5e8f5de1a183e0a81
Andre Guilherme de Oliveira Alcantara <andre.alcantara@reitoria.ifpe.edu.br>
2022-01-13 12:20:48 -0300

    Ajustado relacionamentos

--- apps/registro_hora_extra/views.py
+++ apps/registro_hora_extra/views.py
@@ -1,5 +1,7 @@
 from django.urls import reverse_lazy
 from django.views.generic import ListView, UpdateView, DeleteView, CreateView
+
+from .forms import RegistroHoraExtraForm
 from .models import RegistroHoraExtra
 
 
@@ -23,5 +25,9 @@ class HoraExtraDelete(DeleteView):
 
 class HoraExtraCreate(CreateView):
     model = RegistroHoraExtra
-    fields = ['motivo', 'funcionario', 'hora_extra']
+    form_class = RegistroHoraExtraForm
 
+    def get_form_kwargs(self):
+        kwargs = super(HoraExtraCreate, self).get_form_kwargs()
+        kwargs.update({'user': self.request.user})
+        return kwargs
