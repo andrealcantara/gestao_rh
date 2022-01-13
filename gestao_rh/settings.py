40c7be5f5072719033afb2e5f48b516a8574b984
Andre Guilherme de Oliveira Alcantara <andre.alcantara@reitoria.ifpe.edu.br>
2022-01-13 14:56:58 -0300

    Ajustando settings para adicionar corretamente o paths de static

--- gestao_rh/settings.py
+++ gestao_rh/settings.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import os
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -89,13 +90,15 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 STATICFILES_DIRS = [
     BASE_DIR / "static"
 ]
 
 MEDIA_URL = '/media/'
 
-MEDIA_ROOT = BASE_DIR/"media"
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'login'
